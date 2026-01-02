e54f87705841c7ba8a396e14c3febf0446792f33
Hames <15520815498@163.com>
2026-01-02 12:40:24 +0800

    新增计时器

--- Means/High-precision_Adder/Fibonacci.cpp
+++ Means/High-precision_Adder/Fibonacci.cpp
@@ -2,7 +2,9 @@
 
 #include<vector>
 #include<iostream>
+#include<chrono>
 #include"High-precision_Adder.h"
+using namespace std::chrono;
 int main()
 {
     int n;
@@ -15,18 +17,22 @@ int main()
         std::vector<int> pre_1 = {1};
         std::vector<int> pre_2 = {0};
         std::vector<int> current;
+        auto start = high_resolution_clock::now();
         for (int i = 0; i < n - 1;i++)
         {
             current = add(pre_1, pre_2);
             pre_2 = pre_1;
             pre_1 = current;
         }
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
         int len = current.size();
         std::cout << "第" << n << "项是: ";
         for (int i = len - 1; i >= 0;i--)
         {
             std::cout << current[i] << (i == 0 ? "\n" : "");
         }
+        std::cout << "本次计算耗时：" << duration.count() << " us(微秒)" << std::endl;
     }
     return 0;
 }
